0a2d1c025280ff09cc64e813b5855fa521a459ef
Liam Silk <liam.j.silk@gmail.com>
2016-01-30 15:45:37 +0000

    Added edit & destroy in post controller and added edit links to show page

--- insta/app/controllers/posts_controller.rb
+++ insta/app/controllers/posts_controller.rb
@@ -16,6 +16,17 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
   end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    @post.update(post_params)
+    redirect_to(post_path(@post))
+  end
+
 private
 
   def post_params
